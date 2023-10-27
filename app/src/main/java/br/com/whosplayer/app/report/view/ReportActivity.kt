7c63f5765f47a4483bab6d7bb006aedfcaa2f3db
Daniel Escobar <dandan.scb@hotmail.com>
2023-10-27 09:10:03 -0300

    added a email with link in ReportActivity

--- app/src/main/java/br/com/whosplayer/app/report/view/ReportActivity.kt
+++ app/src/main/java/br/com/whosplayer/app/report/view/ReportActivity.kt
@@ -3,9 +3,13 @@ package br.com.whosplayer.app.report.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import br.com.whosplayer.databinding.ActivityWhosPlayerReportBinding
-
 
 class ReportActivity : AppCompatActivity() {
 
@@ -22,10 +26,25 @@ class ReportActivity : AppCompatActivity() {
         binding.closeButton.setOnClickListener {
             finish()
         }
+
+        val spannableString = SpannableString(binding.whosPlayerEmail.text)
+
+        spannableString.setSpan(
+            object : ClickableSpan() {
+                override fun onClick(textView: View) {
+                    // ToDo
+                }
+            }, START_INDEX, binding.whosPlayerEmail.text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.whosPlayerEmail.text = spannableString
+        binding.whosPlayerEmail.movementMethod = LinkMovementMethod.getInstance()
     }
 
 
     companion object {
+
+        private const val START_INDEX = 0
 
         @JvmStatic
         fun newInstance(context: Context): Intent = Intent(context, ReportActivity::class.java)
