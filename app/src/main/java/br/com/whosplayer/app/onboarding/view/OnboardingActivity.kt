72c82e0ad1603929ef89f81cad0c0ded970c3d9b
Daniel Escobar <dandan.scb@hotmail.com>
2023-10-30 23:53:06 -0300

    removed not used imports

--- app/src/main/java/br/com/whosplayer/app/onboarding/view/OnboardingActivity.kt
+++ app/src/main/java/br/com/whosplayer/app/onboarding/view/OnboardingActivity.kt
@@ -4,7 +4,6 @@ import android.app.ActionBar
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
