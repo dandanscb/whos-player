6d20bc3b1b21d9b283130a66d81bdfced962bc49
Daniel Escobar <dandan.scb@hotmail.com>
2023-11-01 15:47:10 -0300

    adjust report screen

--- app/src/main/java/br/com/whosplayer/app/report/view/ReportActivity.kt
+++ app/src/main/java/br/com/whosplayer/app/report/view/ReportActivity.kt
@@ -5,6 +5,8 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.whosplayer.R
@@ -13,6 +15,8 @@ import br.com.whosplayer.databinding.ActivityWhosPlayerReportBinding
 class ReportActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWhosPlayerReportBinding
+    private var startY = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
@@ -37,6 +41,23 @@ class ReportActivity : AppCompatActivity() {
             Toast.makeText(this, "Texto copiado para a área de transferência", Toast.LENGTH_SHORT)
                 .show()
         }
+
+        binding.reportCardContainer.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        startY = event.y
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        val deltaY = event.y - startY
+                        val newTopMargin = (view.top + deltaY).toInt()
+                        view.layout(view.left, newTopMargin, view.right, newTopMargin + view.height)
+                        startY = event.y
+                    }
+                }
+                return true
+            }
+        })
     }
 
 
